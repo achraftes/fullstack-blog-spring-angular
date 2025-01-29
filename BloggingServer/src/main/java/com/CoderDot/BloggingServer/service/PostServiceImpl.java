d19d5a8f720753c51d4cf37d731aa1d94a66fddb
achraf <achraf04chke@gmail.com>
2025-01-29 04:30:38 +0100

    Added LikePost functionality to increment post like count

--- BloggingServer/src/main/java/com/CoderDot/BloggingServer/service/PostServiceImpl.java
+++ BloggingServer/src/main/java/com/CoderDot/BloggingServer/service/PostServiceImpl.java
@@ -44,4 +44,18 @@ public class PostServiceImpl implements PostService {
             throw new EntityNotFoundException("Post not found");
         }
     }
+    public void LikePost(long postId){
+        Optional<Post> optionalPost = postRepository.findById(postId);
+        if(optionalPost.isPresent()){
+            Post post = optionalPost.get();
+
+            post.setLikeCount(post.getLikeCount()+1);
+            postRepository.save(post);
+
+        } else{
+            throw new EntityNotFoundException("Post not found");
+        }
+
+    }
+
 }
