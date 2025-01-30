35694dbe4b2013e90f5fbb1554c3fa624bac3fe1
achraf <achraf04chke@gmail.com>
2025-01-30 05:23:59 +0100

    Add HTML structure and CSS styling for home

--- bloggingWeb/src/app/app.routes.ts
+++ bloggingWeb/src/app/app.routes.ts
@@ -3,8 +3,11 @@ import { CreatePostComponent } from './pages/create-post/create-post.component';
 import { ViewAllComponent } from './pages/view-all/view-all.component';
 import { ViewPostComponent } from './pages/view-post/view-post.component';
 import { SearchByNameComponent } from './pages/search-by-name/search-by-name.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
+  { path: '',
+     component: HomeComponent }, 
   { 
     path: 'create-post', 
     component: CreatePostComponent 
