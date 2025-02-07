a57f396fd43e6f7c3cec7dc8e0069464b0086884
achraf <achraf04chke@gmail.com>
2025-02-07 02:31:05 +0100

    Updated Home and ViewPost pages with HTML and CSS changes

--- bloggingWeb/src/app/pages/home/home.component.ts
+++ bloggingWeb/src/app/pages/home/home.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [
+    MatIconModule,
+    RouterModule
+
+    
+  ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
